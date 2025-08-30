becce0e66e05f9ba1fbd3fc05c8c73585a644462
satyam kr <satyam.9242@gmail.com>
2025-08-30 06:40:13 +0530

    server setup with database connection

---
+++ src/test/java/com/atomTechAssig/entries/EntriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomTechAssig.entries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
