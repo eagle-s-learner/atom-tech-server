b16bd56d42f7a70e9f37c2414c3b17ede12ce025
satyam kr <satyam.9242@gmail.com>
2025-08-30 09:03:20 +0530

    add controller for user

--- src/main/java/com/atomTechAssig/entries/controller/UserController.java
+++ src/main/java/com/atomTechAssig/entries/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.atomTechAssig.entries.controller;
 
+import com.atomTechAssig.entries.user.dto.UserDto;
+import com.atomTechAssig.entries.user.service.UserService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("")
+    public UserDto createUser(@RequestBody UserDto userDto){
+        return userService.createUser(userDto);
+    }
+    @GetMapping("")
+    public List<UserDto> getAllUsers(){
+        return userService.getAllUsers();
+    }
 }
