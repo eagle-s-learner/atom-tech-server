d34e504e463bce8a2584a2438aaea81d3170a6dc
satyam kr <satyam.9242@gmail.com>
2025-08-30 07:45:52 +0530

    Add UserDto.java DTO

---
+++ src/main/java/com/atomTechAssig/entries/user/dto/UserDto.java
@@ -0,0 +1,56 @@
+package com.atomTechAssig.entries.user.dto;
+
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    public UserDto() {}
+
+    public UserDto(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public interface FirstNameStep {
+        LastNameStep withFirstName(String firstName);
+    }
+
+    public interface LastNameStep {
+        BuildStep withLastName(String lastName);
+    }
+
+    public interface BuildStep {
+        UserDto build();
+    }
+
+    public static class Builder implements FirstNameStep, LastNameStep, BuildStep {
+        private String firstName;
+        private String lastName;
+
+        private Builder() {
+        }
+
+        public static FirstNameStep user() {
+            return new Builder();
+        }
+
+        @Override
+        public LastNameStep withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        @Override
+        public BuildStep withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        @Override
+        public UserDto build() {
+            return new UserDto(
+                this.firstName,
+                this.lastName
+            );
+        }
+    }
+}
