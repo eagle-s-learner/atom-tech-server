3f1e1e62beb7a867d9afdb73ca5a56a9903c8363
satyam kr <satyam.9242@gmail.com>
2025-08-30 08:29:20 +0530

    add service implementation for get users and create user

--- src/main/java/com/atomTechAssig/entries/user/repository/UserRepository.java
+++ src/main/java/com/atomTechAssig/entries/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.atomTechAssig.entries.user.repository;
 
 import com.atomTechAssig.entries.user.domain.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByOrderByIdDesc();
 }
--- src/main/java/com/atomTechAssig/entries/user/service/UserServiceImpl.java
+++ src/main/java/com/atomTechAssig/entries/user/service/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.atomTechAssig.entries.user.service;
 
+import com.atomTechAssig.entries.user.converter.UserConverter;
+import com.atomTechAssig.entries.user.domain.User;
 import com.atomTechAssig.entries.user.dto.UserDto;
 import com.atomTechAssig.entries.user.repository.UserRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,21 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserConverter userConverter;
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+        User user = userConverter.convertFrom(userDto);
+
+        User savedUser = userRepository.save(user);
+
+        return userConverter.convertTo(savedUser);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
-        return null;
+        return userRepository.findAllByOrderByIdDesc().stream()
+            .map(userConverter::convertTo)
+            .collect(Collectors.toList());
     }
 }
